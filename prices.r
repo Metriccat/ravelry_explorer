a43c9bc34bda900e8fdcce218df33a8d951cba8b
saskia bosma <saskia.bosma@acsfrance.com>
2015-09-09 17:32:35 +0200

    fixed bug in prices.r, histogram now correct

--- prices.r
+++ prices.r
@@ -4,10 +4,11 @@ library(httr)
 library(XML)
 library(reshape2)
 library(quantmod)
+library(ggplot2)
 
 categories = c("hat","sweater","neck-torso","feet-legs","hands","home","toysandhobbies","pattern-component","pet")
 
-search_url = "https://api.ravelry.com/patterns/search.json?page_size=5000&sort=projects&craft=knitting&availability=ravelry%2B-free&pc="
+search_url = "https://api.ravelry.com/patterns/search.json?page_size=2000&sort=projects&craft=knitting&availability=ravelry%2B-free&pc="
 
 cat_search <- sapply(categories, function(name) paste(search_url,name,sep="",collapse=""))
 
@@ -22,11 +23,16 @@ permalinks <- melt(permalinks)
 names(permalinks) <- c("link","category")
 
 permalinks_full <- sapply(permalinks$link, function(name) paste("http://www.ravelry.com/patterns/library/",name,sep="",collapse=""))
-               
+
+samp = sample(1:length(permalinks$link),length(permalinks$link))
+permalinks_full <- permalinks_full[samp]    #random sampling    
+permalinks <- permalinks[samp,]    #random sampling    
+
 # web scraping to get the price from the pattern page
 # about 1 min for 50 links
-pattern_info <- lapply(permalinks_full, htmlTreeParse, useInternalNodes = TRUE)
-names(pattern_info) <- permalinks$link
+n=10000 # 1000 ok
+pattern_info <- lapply(permalinks_full[1:n], htmlTreeParse, useInternalNodes = TRUE)
+names(pattern_info) <- permalinks$link[1:n]
 # euros conversion only for logged users; price available only for rav downloads
 pattern_prices <- lapply(pattern_info, function(html) getNodeSet(html, 
                                                                  path="//strong[@class='price']/a/text()", 
@@ -37,9 +43,9 @@ num_prices <- lapply(pattern_prices, function(str) c("price"=regmatches(str,
                                                      "currency"=substr(str,nchar(str)-2,nchar(str)) 
                                                      )
                      )
-
+length(pattern_info)
 price_data  <- data.frame(matrix(unlist(num_prices), nrow=length(num_prices), byrow=T), stringsAsFactors=F)
-price_data <- cbind(permalinks[1:45,], price_data )
+price_data <- cbind(permalinks[1:n,], price_data )
 names(price_data) <- c("link", "category", "price", "currency")
 price_data$price <- as.numeric(price_data$price)
 
@@ -49,4 +55,10 @@ for (curr in unique(price_data$currency)) getFX(paste(curr,"/EUR",sep=""),from =
 exchange_rates = sapply(currencies_codes, get)
 price_data$price_eur = price_data$price * exchange_rates
 
+dat <- data.frame(xx = c(runif(100,20,50),runif(100,40,80),runif(100,0,30)),yy = rep(letters[1:3],each = 100))
+categories = c("hat","sweater","neck-torso","feet-legs","hands","home","toysandhobbies","pattern-component","pet")
 
+ggplot(price_data,aes(x=price_eur)) + 
+  geom_histogram(data=subset(price_data, category == categories[1]),fill = "red", alpha = 0.2) +
+  geom_histogram(data=subset(price_data, category == categories[2]),fill = "blue", alpha = 0.2) 
+  
